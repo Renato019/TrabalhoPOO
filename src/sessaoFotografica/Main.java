a65a1b394bc1a0559fdcd23e7503a0aa8c4a6292
Renato019 <renatovitoriom@gmail.com>
2025-04-30 09:07:29 -0300

    Alteração no diretório sessaoFotografica

--- src/sessaoFotografica/Main.java
+++ src/sessaoFotografica/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sessao.fotografica;
+package sessaoFotografica;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;

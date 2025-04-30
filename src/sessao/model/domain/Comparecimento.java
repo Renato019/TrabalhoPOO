f7b71b47380358d61be36d643ca1d691b5813567
Renato019 <renatovitoriom@gmail.com>
2025-04-30 08:56:21 -0300

    Primeiro Commit

---
+++ src/sessao/model/domain/Comparecimento.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sessao.model.domain;
+
+/**
+ *
+ * @author renat
+ */
+public class Comparecimento {
+    
+}

9b47ea48497f7d90b4240de0048a2da461c02ac8
Renato019 <renatovitoriom@gmail.com>
2025-04-30 12:05:47 -0300

    Add: tela inicial em FXML

--- src/javafx/model/database/DatabaseMySQL.java
+++ src/javafx/model/database/DatabaseMySQL.java
@@ -18,7 +18,7 @@ public class DatabaseMySQL implements Database {
     public Connection conectar() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            this.connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/simulado2", "postgres","postgres");
+            this.connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/simulado2", "postgres","1234");
             return this.connection;
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(DatabasePostgreSQL.class.getName()).log(Level.SEVERE, null, ex);
